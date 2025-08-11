a954738c1ee07f0a73ee0bf165b09c1745ddf665
Shaan <shaanduss@gmail.com>
2025-08-11 18:01:08 +0800

    Add Chart and Fetch Data

--- components/marketDataFetcher.tsx
+++ components/marketDataFetcher.tsx
@@ -1,47 +1,74 @@
 "use client";
-import React, { useState, useEffect, useRef } from "react";
+
+import React, { useState, useRef, useEffect } from "react";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
+import {
+  ChartContainer,
+  ChartTooltip,
+  ChartLegend,
+  ChartLegendContent,
+  type ChartConfig,
+} from "@/components/ui/chart";
+import { Line, LineChart, CartesianGrid, XAxis, YAxis } from "recharts";
+import { CustomTooltip } from "@/components/customTooltip";
 
 export default function MarketDataFetcher() {
   const [symbol, setSymbol] = useState("^BSESN");
   const [data, setData] = useState<any>(null);
+  const [history, setHistory] = useState<{ time: string; price: number }[]>([]);
   const [loading, setLoading] = useState(false);
   const wsRef = useRef<WebSocket | null>(null);
 
-  // Subscribe to WS on Click
   const startWebSocket = () => {
     setLoading(true);
-    if (wsRef.current) {
-      wsRef.current.close();
-    }
+    if (wsRef.current) wsRef.current.close();
     const ws = new window.WebSocket("ws://localhost:4000");
     wsRef.current = ws;
-    ws.onopen = () => {
-      ws.send(JSON.stringify({ symbol }));
-    };
+
+    ws.onopen = () => ws.send(JSON.stringify({ symbol }));
+
     ws.onmessage = (event) => {
-      setData(JSON.parse(event.data));
-      setLoading(false);
+      const msg = JSON.parse(event.data);
+      if (msg.type === "history") {
+        console.log(msg.data);
+        setHistory(msg.data);
+        setLoading(false);
+      } else if (msg.type === "live") {
+        setData(msg.data);
+
+        setHistory((h) => [
+          ...h,
+          {
+            time: new Date().toISOString(),
+            price: msg.data.price,
+          },
+        ]);
+      } else if (msg.type === "error") {
+        setLoading(false);
+      }
     };
+
     ws.onerror = () => {
       alert("WebSocket error");
       setLoading(false);
     };
   };
 
-  // Component Unmount handling
-  useEffect(() => {
-    return () => {
-      if (wsRef.current) {
-        wsRef.current.close();
-      }
-    };
-  }, []);
+  useEffect(() => () => wsRef.current?.close(), []);
+
+  const chartConfig: ChartConfig = {
+    price: {
+      label: "Market Price",
+    },
+  };
+
+  const chartData = history.slice(-200);
 
   return (
     <div className="p-4">
-      <div className="flex gap-x-7">
+      <h1 className="font-bold text-xl">STOCK SEARCH</h1>
+      <div className="flex gap-x-7 mt-2">
         <Input
           type="text"
           value={symbol}
@@ -49,7 +76,11 @@ export default function MarketDataFetcher() {
           className="border rounded p-2 mb-2"
           placeholder="Enter stock symbol (e.g. MSFT)"
         />
-        <Button onClick={startWebSocket} disabled={loading}>
+        <Button
+          onClick={startWebSocket}
+          disabled={loading}
+          className="cursor-pointer"
+        >
           {loading ? "Subscribing..." : "Subscribe"}
         </Button>
       </div>
@@ -58,11 +89,39 @@ export default function MarketDataFetcher() {
           <h3>
             {data.shortName} ({data.symbol})
           </h3>
-          <p>Price: {data.regularMarketPrice}</p>
-          <p>
-            Change: {data.regularMarketChange} (
-            {data.regularMarketChangePercent}%)
-          </p>
+          <p>Price: {data.price}</p>
+        </div>
+      )}
+      {chartData.length > 1 && (
+        <div className="mt-8">
+          <ChartContainer
+            config={chartConfig}
+            className="min-h-[240px] max-h-[400px] w-full"
+          >
+            <LineChart data={chartData}>
+              <CartesianGrid strokeDasharray="3 3" />
+              <XAxis
+                dataKey="time"
+                tickFormatter={(value) => {
+                  const dateObj = new Date(value);
+                  return dateObj.toLocaleTimeString([], {
+                    hour: "2-digit",
+                    minute: "2-digit",
+                  });
+                }}
+              />
+              <YAxis domain={["auto", "auto"]} />
+              <ChartTooltip content={<CustomTooltip />} />
+              <ChartLegend content={<ChartLegendContent />} />
+              <Line
+                type="monotone"
+                dataKey="price"
+                stroke="var(--chart-1)"
+                strokeWidth={3}
+                dot={false}
+              />
+            </LineChart>
+          </ChartContainer>
         </div>
       )}
     </div>
