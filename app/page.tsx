f15ef32da59a23b85d45e776d957e0dfe086da5a
Shaan <shaanduss@gmail.com>
2025-08-15 15:11:06 +0800

    Fix: Width Styling Issue

--- app/page.tsx
+++ app/page.tsx
@@ -3,25 +3,23 @@ import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
 export default function Home() {
   return (
-    <div className="flex justify-items items-center px-20 mt-10">
-      <div>
-        <Tabs defaultValue="yfinance">
-          <TabsList>
-            <TabsTrigger value="yfinance" className="cursor-pointer">
-              Yahoo Finance
-            </TabsTrigger>
-            <TabsTrigger value="alpha-vantage" className="cursor-pointer">
-              Alpha Vantage
-            </TabsTrigger>
-          </TabsList>
-          <TabsContent value="yfinance">
-            <MarketDataFetcher platform="yfinance"/>
-          </TabsContent>
-          <TabsContent value="alpha-vantage">
-            <MarketDataFetcher platform="alpha-vantage"/>
-          </TabsContent>
-        </Tabs>
-      </div>
+    <div className="flex justify-items items-center px-20 mt-10 w-full">
+      <Tabs defaultValue="yfinance" className="w-full">
+        <TabsList>
+          <TabsTrigger value="yfinance" className="cursor-pointer">
+            Yahoo Finance
+          </TabsTrigger>
+          <TabsTrigger value="alpha-vantage" className="cursor-pointer">
+            Alpha Vantage
+          </TabsTrigger>
+        </TabsList>
+        <TabsContent value="yfinance">
+          <MarketDataFetcher platform="yfinance" />
+        </TabsContent>
+        <TabsContent value="alpha-vantage">
+          <MarketDataFetcher platform="alpha-vantage" />
+        </TabsContent>
+      </Tabs>
     </div>
   );
 }
